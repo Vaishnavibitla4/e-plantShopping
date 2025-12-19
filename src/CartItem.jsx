7458fafe2d4a6713247f6c17fd515c0e92d0bb9e
Vaishnavibitla4 <bitlavaishnavi4@gmail.com>
2025-12-19 04:34:07 -0500

    Completed CartItem functionality with real-time updates

--- src/CartItem.jsx
+++ src/CartItem.jsx
@@ -9,27 +9,56 @@ const CartItem = ({ onContinueShopping }) => {
 
   // Calculate total amount for all products in the cart
   const calculateTotalAmount = () => {
- 
+         let total = 0;
+
+    cart.forEach(item => {
+      const price = parseFloat(item.cost.substring(1)); // remove "$"
+      total += price * item.quantity;
+    });
+
+    return total.toFixed(2);
   };
 
   const handleContinueShopping = (e) => {
-   
+     onContinueShopping(e);
   };
 
 
 
   const handleIncrement = (item) => {
+    dispatch(
+    updateQuantity({
+      name: item.name,
+      quantity: item.quantity + 1,
+    })
+  );
   };
 
   const handleDecrement = (item) => {
-   
+   if (item.quantity > 1) {
+    // Decrease quantity by 1
+    dispatch(
+      updateQuantity({
+        name: item.name,
+        quantity: item.quantity - 1,
+      })
+    );
+  } else {
+    // If quantity becomes 0, remove item from cart
+    dispatch(removeItem(item.name));
+  }
   };
-
+  const handleCheckoutShopping = (e) => {
+  alert('Functionality to be added for future reference');
+};
   const handleRemove = (item) => {
+         dispatch(removeItem(item.name));
   };
 
   // Calculate total cost based on quantity for an item
   const calculateTotalCost = (item) => {
+    const price = parseFloat(item.cost.substring(1)); // Remove "$" and convert to number
+  return (price * item.quantity).toFixed(2);
   };
 
   return (
